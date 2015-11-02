37ec3ce8cbf9c37948c3c70aff386175f65880ab
Alexander Arevalo Garavito <lex_94@outlook.com>
2015-11-01 22:21:29 -0500

    controlador login

--- application/controllers/clogin.php
+++ application/controllers/clogin.php
@@ -11,21 +11,28 @@ class Clogin extends CI_Controller{
 	function index(){
 		$this->load->view('logueo.html');
 	}
-
+	function validar(){
+		$this->form_validation->set_rules('usuario', 'Usuario', 'trim|required|max_length[15]');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required|max_length[20]');
+	}
 	function loguear(){
 		$_POST=json_decode(file_get_contents('php://input'),TRUE);
 		$user=$this->input->post('usuario');
 		$password=$this->input->post('password');
-		$dato=$this->modelo->loguear($user,$password);
-		settype($dato['0'],"array");
-		if ($dato!=null) {
-			$tipo=$dato['0']['idtipousuario'];
-			if ($tipo==1) {
-				redirect('cProducto');
-			} else if ($tipo==2) {
-				redirect('cCliente');
-			} else redirect('cPublico');
-		} else $mensaje['notLogueo']="Usuario o password no son correctas";
+		$this->validar();
+		if ($this->form_validation->run()!=FALSE) {
+			$dato=$this->modelo->loguear($user,$password);
+			settype($dato['0'],"array");
+			if ($dato!=null) {
+				$tipo=$dato['0']['idtipousuario'];
+				if ($tipo==1) {
+					redirect('cProducto');
+				} else if ($tipo==2) {
+					redirect('cCliente');
+				} else redirect('cPublico');
+			} else echo json_encode(array("respuesta" => "Usuario o password no son correctas"));
+		} else echo json_encode(array("respuesta" => "Los datos proporcionados son erróneos"));
+		
 	}
 }	
 ?>
