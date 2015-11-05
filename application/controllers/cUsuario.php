33b14ba886babb9a881b4e0b8314d1b646d03a5c
Alexander Arevalo Garavito <lex_94@outlook.com>
2015-11-05 18:04:30 -0500

    controlador usuario
    controlador usuario

--- application/controllers/cUsuario.php
+++ application/controllers/cUsuario.php
@@ -3,10 +3,10 @@ class CUsuario extends CI_Controller{
 
 	function __construct(){
 		parent:: __construct();
-		$this->load->library('session');
-		$this->load->library('form_validation');
 		$this->load->helper('url');
 		$this->load->helper('security');
+		$this->load->library('session');
+		$this->load->library('form_validation');
 		$this->load->model('modelo');
 	}
 
