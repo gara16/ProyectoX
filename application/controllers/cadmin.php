67e15d6ff66e368c8aa71720b8a8a0d244c4baa1
Mrthnx <mrthnx96@gmail.com>
2015-11-05 20:21:31 -0500

    a

--- application/controllers/cadmin.php
+++ application/controllers/cadmin.php
@@ -1,6 +1,6 @@
 <?php 
 if (!defined('BASEPATH')) exit('No direct script access allowed');
-class Clogin extends CI_Controller{
+class Cadmin extends CI_Controller{
 
 	function __construct(){
 		parent:: __construct();
--- js/factoria.js
+++ js/factoria.js
@@ -3,13 +3,13 @@ var app = angular.module('SVentas');
 app.factory('fagregar', function ($http) {
 	return {
 		flistarT:function(){
-			return $http.get('cproducto/listarTipo');
+			return $http.get('cadmin/listarTipo');
 		},
 		flistarM:function(){
-			return $http.get('cproducto/listarMedida');
+			return $http.get('cadmin/listarMedida');
 		},
 		fagregarP:function($valor){
-			return $http.post('cproducto/agregarProducto',JSON.stringify($valor));
+			return $http.post('cadmin/agregarProducto',JSON.stringify($valor));
 		}
 		
 	};
@@ -18,16 +18,16 @@ app.factory('fagregar', function ($http) {
 app.factory('flistar', function ($http) {
 	return {
 		flistarP:function(){
-			return $http.get('cproducto/listarProducto');
+			return $http.get('cadmin/listarProducto');
 		},
 		fbuscarP:function($valor){
-			return $http.post('cproducto/buscarProducto',JSON.stringify($valor));
+			return $http.post('cadmin/buscarProducto',JSON.stringify($valor));
 		},
 		feliminarP:function($valor){
-			return $http.post('cproducto/eliminarProducto',JSON.stringify($valor));
+			return $http.post('cadmin/eliminarProducto',JSON.stringify($valor));
 		},
 		fmodificarP:function($valor){
-			return $http.post('cproducto/modificarProducto',JSON.stringify($valor));
+			return $http.post('cadmin/modificarProducto',JSON.stringify($valor));
 		}
 	}
 });
